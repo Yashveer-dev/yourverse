02928d44c2e98b040966ed2623e9df5684b13c8d
Yashveer-dev <yashveer.shukla.2006@gmail.com>
2025-08-31 00:48:20 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -4,13 +4,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-storage.js";
+// Firebase Storage is no longer needed, so its import has been removed.
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCtJ8SvQjbeOaXJXggI8AcUARO__cVqzNQ",
   authDomain: "yourverse-77089.firebaseapp.com",
   projectId: "yourverse-77089",
+  // storageBucket is no longer used by the app but can remain in the config.
   storageBucket: "yourverse-77089.firebasestorage.app",
   messagingSenderId: "821758205098",
   appId: "1:821758205098:web:4eb41dc9fec716514a44d3",
@@ -23,4 +24,4 @@ const app = initializeApp(firebaseConfig);
 // Export the services to be used in other files
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const storage = getStorage(app);
+// The 'storage' export has been removed as it is no longer used.
